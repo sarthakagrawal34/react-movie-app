6e81fafd04d22ef4860d517a0a0c678c3a62bb80
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-02-28 13:51:16 +0530

    Start the store and console the store and state

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import { createStore } from 'redux';
 
 import './index.css';
 import App from './components/App';
+import movies from './components/reducers';
 
 
-const store = createStore();
+const store = createStore(movies);
+console.log("Store", store);
+console.log('State', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
