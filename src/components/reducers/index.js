25f08f3b7e92659097d46d36afc0343de6a6575f
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-02-28 16:22:21 +0530

    Changing tabs functionality

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,9 +1,10 @@
-import { ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES } from "../actions";
+import { ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES } from "../actions";
 
 const initialMoviesState = {
     list: [],
     favourites: [],
-}
+    showFavourites: false,
+};
 
 export default function movies(state = initialMoviesState, action) {
     // if (action.type === ADD_MOVIES) {
@@ -36,7 +37,13 @@ export default function movies(state = initialMoviesState, action) {
                 favourites: filteredArray,
             };
         
+        case SET_SHOW_FAVOURITES:
+            return {
+                ...state,
+                showFavourites: action.val,
+            }
+        
         default:
             return state;
-    }
-}
+    };
+};
