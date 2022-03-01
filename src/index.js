24a8737eb02a279c1d6ae1bcae3ae59151de0ea5
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-03-01 14:44:54 +0530

    Apply middleware to the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './components/App';
 import combineReducers from './components/reducers';
 
+// currying concept used here
+// function logger (obj,next,action) = logger (obj)(next)(action)
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      // middleware code
+      console.log("ACTION_TYPE", action.type);
+      next(action);
+    };
+  };
+};
 
-const store = createStore(combineReducers);
+
+const store = createStore(combineReducers, applyMiddleware(logger));
 console.log("Store", store);
 // console.log('Before State', store.getState());
 
